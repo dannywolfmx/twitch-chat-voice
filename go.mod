c58e2c6f1d17041ebe35d89a2d488c99908679ca
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2022-08-13 12:15:18 -0500

    Fix race condition on close the player
    * Using the new go-tts, we can know when to stop the player without race condition
    
    * Using the new go-tts, we can know when to stop the player without race condition

--- go.mod
+++ go.mod
@@ -3,12 +3,21 @@ module github.com/dannywolfmx/twitch-chat-voice
 go 1.18
 
 require (
-	github.com/dannywolfmx/go-tts v0.0.2
+	gioui.org v0.0.0-20220808141521-a55065af9c1e
+	github.com/dannywolfmx/go-tts v0.0.8
 	github.com/gempir/go-twitch-irc/v3 v3.2.0
 )
 
 require (
+	gioui.org/cpu v0.0.0-20210817075930-8d6a761490d2 // indirect
+	gioui.org/shader v1.0.6 // indirect
+	github.com/benoitkugler/textlayout v0.1.1 // indirect
+	github.com/gioui/uax v0.2.1-0.20220325163150-e3d987515a12 // indirect
+	github.com/go-text/typesetting v0.0.0-20220411150340-35994bc27a7b // indirect
 	github.com/hajimehoshi/go-mp3 v0.3.3 // indirect
 	github.com/hajimehoshi/oto/v2 v2.2.0 // indirect
+	golang.org/x/exp v0.0.0-20210722180016-6781d3edade3 // indirect
+	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
 	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -2,39 +2,102 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
+	"image/color"
 
+	"gioui.org/app"
+	"gioui.org/font/gofont"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/text"
+	"gioui.org/unit"
+	"gioui.org/widget/material"
 	"github.com/dannywolfmx/go-tts/tts"
 	"github.com/gempir/go-twitch-irc/v3"
 )
 
+var screenText = make(chan string)
+var done = make(chan bool)
+
+var texto string
+
 func main() {
+	twitchChannelName := "dannywolfmx2"
 	client := twitch.NewAnonymousClient()
-	player := tts.NewTTS()
-	go func() {
-		player.Run()
-	}()
+	player := tts.NewTTS("es")
 	//client := twitch.NewClient("yourtwitchusername", "oauth:123123123")
 
 	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
-		fmt.Println(message.Message)
-		player.Play("es", message.Message)
+		m := fmt.Sprintf("%s: %s \n", message.User.Name, message.Message)
+		player.Play(m)
 	})
 
-	client.Join("dannywolfmx2")
-
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt)
 	go func() {
-		for {
+		for playing := range player.Playing() {
 			select {
-			case <-quit:
-				player.Stop()
-				client.Disconnect()
+			case <-player.Done:
+				return
+			default:
+				screenText <- playing.GetText()
+				playing.Play()
 			}
 		}
 	}()
 
+	client.Join(twitchChannelName)
+
+	client.Say(twitchChannelName, "/vips")
+
+	go func() {
+		w := app.NewWindow()
+		run(w, player, client)
+	}()
+
 	fmt.Println(client.Connect())
+}
+
+func run(w *app.Window, player *tts.TTS, client *twitch.Client) error {
+
+	theme := material.NewTheme(gofont.Collection())
+	var ops op.Ops
+
+	for {
+		select {
+		case e := <-w.Events():
+			switch e := e.(type) {
+
+			case system.DestroyEvent:
+				client.Disconnect()
+				player.Stop()
+				return e.Err
+			case system.FrameEvent:
+				Layout(theme, &ops, e)
+
+			}
+		case m := <-screenText:
+			texto = m
+			w.Invalidate()
+		}
+	}
+}
+
+func Layout(theme *material.Theme, ops *op.Ops, e system.FrameEvent) {
+	gtx := layout.NewContext(ops, e)
+	layout.Flex{
+		Axis:    layout.Vertical,
+		Spacing: layout.SpaceStart,
+	}.Layout(gtx,
+		layout.Rigid(layout.Spacer{Height: unit.Dp(25)}.Layout),
+		layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+			title := material.H1(theme, texto)
+			maroon := color.NRGBA{R: 127, A: 255}
+
+			title.Color = maroon
+			title.Alignment = text.Middle
+			return title.Layout(gtx)
+		}),
+		layout.Rigid(layout.Spacer{Height: unit.Dp(25)}.Layout),
+	)
+
+	e.Frame(gtx.Ops)
 }
