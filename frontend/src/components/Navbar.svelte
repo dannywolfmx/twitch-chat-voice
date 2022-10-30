027bba7fd33cef67c85ae97f34c76d55b7bf0f86
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2022-10-30 16:36:13 -0600

    Change navbar

--- frontend/src/components/Navbar.svelte
+++ frontend/src/components/Navbar.svelte
@@ -3,24 +3,24 @@
     export let selectedPage;
 </script>
 
-<div class="bg-gray-900 sm:p-2 flex sm:gap-2">
+<div class="bg-gray-900 p-2 flex gap-2">
     <button
         on:click={() => (selectedPage = "Main")}
         class="{selectedPage == 'Main'
-            ? 'bg-purple-800'
-            : 'bg-gray-800 hover:bg-purple-800'} grow min-w-20 max-w-30 h-full sm:rounded-2 p-3"
+            ? 'bg-purple-900'
+            : 'bg-gray-800 hover:bg-purple-900'} grow min-w-12 max-w-30 rounded-2 p-3"
     >
         <i class="i-tabler-message-circle-2 h-full w-full text-purple-200" />
     </button>
-    <div class="sm:grow" />
+    <div class="grow" />
     <Player />
-    <div class="sm:grow" />
+    <div class="grow" />
     <button
         on:click={() => (selectedPage = "Config")}
         class="{selectedPage === 'Config'
-            ? 'bg-purple-800'
-            : 'bg-gray-800 hover:bg-purple-800'} grow min-w-20 max-w-30 h-full sm:rounded-2 p-3"
+            ? 'bg-blue-900'
+            : 'bg-gray-800 hover:bg-blue-900'} grow min-w-12 max-w-30 rounded-2 p-3"
     >
-        <i class="i-tabler-settings h-full w-full text-purple-200" />
+        <i class="i-tabler-settings h-full w-full text-blue-200" />
     </button>
 </div>
--- frontend/src/components/Player.svelte
+++ frontend/src/components/Player.svelte
@@ -19,26 +19,26 @@
     }
 </script>
 
-<div class="flex justify-between grow min-w-40">
+<div class="flex justify-between grow min-w-36 max-w-64">
     {#if pauseState}
         <button
-            class="grow sm:rounded-l-2 bg-gray-700 p-3"
+            class="grow rounded-l-2 bg-gray-700 p-3 animate-pulse"
             on:click={() => resumeEvent()}
         >
-            <i class="i-tabler-play text-purple-200 h-full w-full" />
+            <i class="i-tabler-play text-purple-200 h-full w-full " />
         </button>
     {:else}
         <button
-            class="grow sm:rounded-l-2 bg-gray-700 p-3"
+            class="grow rounded-l-2 bg-gray-700 p-3"
             on:click={() => pauseEvent()}
         >
             <i class="i-tabler-pause text-purple-200 h-full w-full" />
         </button>
     {/if}
     <button
-        class="sm:rounded-r-2 bg-gray-600 p-3 grow"
+        class="rounded-r-2 bg-gray-600 p-3 grow hover:bg-gray-700 "
         on:click={() => nextEvent()}
     >
-        <i class="i-tabler-player-track-next text-purple-200 h-full w-full" />
+        <i class="i-tabler-player-track-next text-purple-200 h-full w-full " />
     </button>
 </div>
