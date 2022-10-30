fc6b41d0a6cd2fd739031687e4ef22860247fbfd
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2022-10-29 20:33:19 -0500

    Better loggin

--- frontend/src/components/Navbar.svelte
+++ frontend/src/components/Navbar.svelte
@@ -8,12 +8,10 @@
         on:click={() => (selectedPage = "Main")}
         class="{selectedPage == 'Main'
             ? 'bg-purple-800'
-            : 'bg-gray-800 hover:bg-purple-800'} grow min-w-20 h-full sm:rounded-2 p-3"
+            : 'bg-gray-800 hover:bg-purple-800'} grow min-w-20 max-w-30 h-full sm:rounded-2 p-3"
     >
-        <i
-            class="i-tabler-message-circle-2 h-full w-full text-purple-200"
-        /></button
-    >
+        <i class="i-tabler-message-circle-2 h-full w-full text-purple-200" />
+    </button>
     <div class="sm:grow" />
     <Player />
     <div class="sm:grow" />
@@ -21,7 +19,8 @@
         on:click={() => (selectedPage = "Config")}
         class="{selectedPage === 'Config'
             ? 'bg-purple-800'
-            : 'bg-gray-800 hover:bg-purple-800'} grow min-w-20 h-full sm:rounded-2 p-3"
-        ><i class="i-tabler-settings h-full w-full text-purple-200" /></button
+            : 'bg-gray-800 hover:bg-purple-800'} grow min-w-20 max-w-30 h-full sm:rounded-2 p-3"
     >
+        <i class="i-tabler-settings h-full w-full text-purple-200" />
+    </button>
 </div>
--- frontend/src/components/config/Loggin.svelte
+++ frontend/src/components/config/Loggin.svelte
@@ -14,10 +14,10 @@
 </script>
 
 <div
-    class="flex lg:flex-row flex-col gap-10 items-center max-w-200 m-auto p-10 h-full "
+    class="flex md:flex-row flex-col gap-10 items-center max-w-264 m-auto p-10 h-full "
 >
     <div
-        class="flex lg:flex-col gap-8 lg:gap-20 grow items-center lg:w-64 bg-purple-900 p-4 rounded-2"
+        class="flex flex-col gap-20 grow items-center min-w-64 w-full bg-purple-900 p-4 rounded-2"
     >
         <div class="h-32 w-32 rounded-2 p-2">
             <i class="i-tabler-brand-twitch h-full w-full text-purple-200" />
@@ -26,27 +26,26 @@
             class="w-full bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
             on:click={save}
         >
-            <i class="i-tabler-brand-twitch" />
             Conectar con twitch
         </button>
     </div>
     <div
-        class="flex  lg:flex-col gap-8 lg:gap-20 grow items-center lg:w-64  bg-gray-900 p-4 rounded-2"
+        class="flex flex-col  gap-20 grow items-center min-w-64 w-full bg-gray-900 p-4 rounded-2"
     >
         <div class="h-32 w-32 rounded-2 p-2">
             <i class="i-tabler-spy h-full w-full text-gray-200" />
         </div>
-        <div class="flex w-full lg:flex-row flex-col">
+        <div class="flex w-full md:flex-row flex-col">
             <input
                 type="text"
                 id="first_name"
-                class="bg-gray-600  font-bold text-gray-50 text-sm lg:rounded-l-2 lg:rounded-r-0 rounded-t-2 focus:ring-blue-500 focus:border-blue-500 block p-2.5 grow"
+                class="bg-gray-600  font-bold text-gray-50 text-sm md:rounded-l-2 md:rounded-r-0 rounded-t-2 focus:ring-blue-500 focus:border-blue-500 p-2.5 grow"
                 placeholder="Modo incógnito"
                 bind:value={channel}
                 required
             />
             <button
-                class="bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 lg:rounded-r-2 lg:rounded-l-0 rounded-b-2 grow"
+                class="bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 md:rounded-r-2 md:rounded-l-0 rounded-b-2 grow"
                 on:click={save}
             >
                 Conectar
